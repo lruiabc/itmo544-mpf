404b45b866a67e4b0f3bc529215ca5290ee26a71
lruiabc <568047669@qq.om>
2015-11-30 15:38:30 -0600

    added a new setup.php

--- setup.php
+++ setup.php
@@ -2,33 +2,6 @@
 require 'vendor/autoload.php';
 use Aws\Rds\RdsClient;
 
-/*
-$link = mysqli_connect("itmo544grh-mp1.ckh0wwv6itjw.us-west-2.rds.amazonaws.com","rui","110224Fish","itmoruidb") or die("Error " . mysqli_error($link));
-if (mysqli_connect_errno()) {
-    printf("Connect failed: %s\n", mysqli_connect_error());
-    exit();
-}
-$sql = "CREATE TABLE comments 
-(
-    id INT NOT NULL AUTO_INCREMENT,
-    uname VARCHAR(20),
-    email VARCHAR(200) NOT NULL,
-    phone VARCHAR(20) NOT NULL,
-    filename VARCHAR(255) NOT NULL,
-    s3rawurl VARCHAR(255) NOT NULL,
-    s3finishedurl VARCHAR(255) NOT NULL,
-    jpg_filename VARCHAR(255),
-    status INT NOT NULL,
-    issubscribed INT NOT NULL,
-    timestamp dtaetime,
-    PRIMARY KEY(id)
-)";
-$con=$link->query($sql);
-
-$client = RdsClient::factory(array(
-'version' =>'latest',
-'region'  => 'us-west-2'
-));*/
 
 $client = RdsClient::factory(array(
 'version' =>'latest',
@@ -87,5 +60,7 @@ $link->close();
 
 ?>
                                                                                                                                                                                  
-~                                                                                                                                                                                                            
-~       
+<h1>go back to index.html</h1>
+<form action="index.html">
+<input type="submit" value="Submit">
+</form>      
