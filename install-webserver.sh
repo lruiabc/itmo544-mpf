5d6f68fcabfde57fc5516e24561e4ad667d28145
lruiabc <568047669@qq.om>
2015-11-29 20:21:15 -0600

    added a new isntall-webserver.sh

--- install-webserver.sh
+++ install-webserver.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 sudo apt-get update -y
-sudo apt-get install -y apache2 git php5 php5-curl mysql-client curl php5-mysql php5-imagick install libmagickwand-dev
+sudo apt-get install -y apache2 git php5 php5-curl mysql-client curl php5-mysql php5-imagick libmagickwand-dev
 
-git clone https://github.com/lruiabc/itmo544-mp2.git
+git clone https://github.com/lruiabc/itmo544-mpf.git
 
-mv ./../itmo544-mp2/images /var/www/html/images
-mv ./../itmo544-mp2/vendor /var/www/html/vendor
-mv ./../itmo544-mp2/index.html /var/www/html
-mv ./../itmo544-mp2/*.php /var/www/html
-mv ./../itmo544-mp2/*.asp /var/www/html
+mv ./../itmo544-mpf/images /var/www/html/images
+mv ./../itmo544-mpf/vendor /var/www/html/vendor
+mv ./../itmo544-mpf/index.html /var/www/html
+mv ./../itmo544-mpf/*.php /var/www/html
+mv ./../itmo544-mpf/*.asp /var/www/html
 
 curl -sS http://getcomposer.org/installer | php
 php composer.phar require aws/aws-sdk-php
 
 
-sudo mv ./../itmo544-mp2/vendor /var/www/html/vendor
+sudo mv ./../itmo544-mpf/vendor /var/www/html/vendor
 
 sudo php /var/www/html/setup.php
 
