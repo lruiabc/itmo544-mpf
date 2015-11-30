e0c712eaa764b84466a95b73ea8f3e3af592de9f
lruiabc <568047669@qq.om>
2015-11-30 15:33:50 -0600

    added a new gallary.php

--- gallary.php
+++ gallary.php
@@ -47,16 +47,16 @@ if (mysqli_connect_errno()) {
 }
 //below line is unsafe - $email is not checked for SQL injection -- don't do this in real life or use an ORM instead
 $link->real_query("SELECT * FROM items WHERE email = '$email'");
-//$link->real_query("SELECT * FROM items");
+
 $res = $link->use_result();
 echo "Result set order...\n";
 while ($row = $res->fetch_assoc()) {
     echo "<img src =\" " . $row['s3rawurl'] . "\" /><img src =\"" .$row['s3finishedurl'] . "\"/>";
 echo $row['id'] . "Email: " . $row['email'] ."uname:" . $row['uname'];
 
-$image=new Imagick(realpath($row['s3rawurl']));
-$image->thumbnaiImage(100,0);
-echo  $image;
+//$image=new Imagick(realpath($row['s3rawurl']));
+//$image->thumbnaiImage(100,0);
+//echo  $image;
 }
 $link->close();
 
