8b3668dddf586c1e245f565807df934ffb049f4a
lruiabc <568047669@qq.om>
2015-11-29 19:39:54 -0600

    added a new install-webserver.sh

--- install-webserver.sh
+++ install-webserver.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get update -y
-sudo apt-get install -y apache2 git php5 php5-curl mysql-client curl php5-mysql php5-imagick
+sudo apt-get install -y apache2 git php5 php5-curl mysql-client curl php5-mysql php5-imagick install libmagickwand-dev
 
 git clone https://github.com/lruiabc/itmo544-mp2.git
 
